96a0b03ccd787c48a4fed6eee05a0fa8421a3ba2
desislavdlg3 <73930674+desislavdlg3@users.noreply.github.com>
2021-07-15 11:41:07 +0200

    Footer fix + Highlights section fix + Welcome text increased readability
    Welcome text increased readability
    - new background color
    - smaller maximum width
    
    Highlights section fix
    - fixed issue with icons being wide apart, rendering the design not pleasant to look at
    
    New design for the footer
    - more content
    - updated contact information
    - added google map functionality
    - fixed responsiveness
    - general clean-up of styling
    - copyright bar is now part of the footer (something that Alex noted during the exam)
    - better user feedback on the overall design of the footer
    - responsive SoMe buttons

--- scripts/script.js
+++ scripts/script.js
@@ -68,7 +68,9 @@ function showSlides(n) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+
 }
 
 // gallery-script-end
+
+
