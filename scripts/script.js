e254e41d2ad5165f4d1ad10554fd3ddf6efe4904
desislavdlg3 <73930674+desislavdlg3@users.noreply.github.com>
2021-07-18 07:25:56 +0200

    general semantic improvements

--- scripts/script.js
+++ scripts/script.js
@@ -74,3 +74,16 @@ function showSlides(n) {
 // gallery-script-end
 
 
+// navbar-show on scroll up-script
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
